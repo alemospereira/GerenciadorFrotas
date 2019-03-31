3a07ffb7475744b0b21732111946541980cdb678
andre lemos pereira <alptdu@hotmail.com>
2019-03-31 00:35:55 -0300

    Finalizado.

--- DominioFrotas/Interfaces/IAplicacaoBase.cs
+++ DominioFrotas/Interfaces/IAplicacaoBase.cs
@@ -13,9 +13,27 @@ namespace DominioFrotas.Interfaces
         /// </summary>
         /// <param name="item"></param>
         void Adicionar(T item);
+        /// <summary>
+        /// Retorna o item pelo Id informado
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>item</returns>
         T ListarPorId(int id);
+        /// <summary>
+        /// Retorna um lista de itens podendo filtrar pelo chassi
+        /// </summary>
+        /// <param name="filtro"></param>
+        /// <returns>List</returns>
         IEnumerable<T> Listar(string filtro);
+        /// <summary>
+        /// Edita o item com os paramentros informados
+        /// </summary>
+        /// <param name="item"></param>
         void Editar(T item);
+        /// <summary>
+        /// Remove o item com o id informado
+        /// </summary>
+        /// <param name="id"></param>
         void Remover(int id);
         void Dispose();
 
